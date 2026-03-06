7e976c0539b4ad9f38ebe81d95dd4521b4926080
Prathamesh <prathameshrt2004@gmail.com>
2026-03-06 15:17:38 +0530

    Move orders page to react

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -26,7 +26,7 @@ export function HomePage(){
       </div>
 
       <div className="right-section">
-        <a className="orders-link header-link" href="/order">
+        <a className="orders-link header-link" href="/orders">
 
           <span className="orders-text">Orders</span>
         </a>
